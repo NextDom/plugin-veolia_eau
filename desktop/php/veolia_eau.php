af75649010e14f5d271bab2f55236cd96f82430d
Damien Virlouvet <damien.virlouvet@gmail.com>
2017-06-30 22:07:55 +0200

    make tab in config

--- desktop/php/veolia_eau.php
+++ desktop/php/veolia_eau.php
@@ -28,156 +28,153 @@ $eqLogics = eqLogic::byType($plugin->getId());
 
     <!-- Affichage de l'eqLogic sélectionné -->
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-        <form class="form-horizontal">
-            <fieldset>
-                <legend>
-                    <!-- Retour au Général et affichage de la configuration avancée -->
-                    <i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}
-                    <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
-                </legend>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">{{Nom du compte Veolia}}</label>
-                    <div class="col-sm-3">
-                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement Veolia eau}}"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label" >{{Objet parent}}</label>
-                    <div class="col-sm-3">
-                        <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                            <option value="">{{Aucun}}</option>
-                            <?php
-                            foreach (object::all() as $object) {
-                                echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                </div>
-				<div class="form-group">
-					<label class="col-sm-3 control-label">{{Catégorie}}</label>
-					<div class="col-sm-8">
-					<?php
-						foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
-							echo '<label class="checkbox-inline">';
-							echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
-							echo '</label>';
-						}
-					?>
-					</div>
-				</div>
-                <div class="form-group">
-					<label class="col-sm-3 control-label" ></label>
-					<div class="col-sm-9">
-                        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-                        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-					</div>
-				</div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">{{Service}}</label>
-                    <div class="col-sm-3">
-                        <select class="form-control configuration eqLogicAttr" id="configuration_website" data-l1key="configuration" data-l2key="website">
-                        <?php
-                            echo '<option value="1">Veolia</option>';
-                            echo '<option value="2">Veolia Méditerranée</option>';
-                            //echo '<option value="3">Service Eau du Grand Lyon</option>';
-                            echo '<option value="4">Tout sur mon eau / Eau en ligne</option>';
-                         ?>
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">{{Identifiant}}</label>
-                    <div class="col-sm-3">
-                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="login" placeholder="Identifiant"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">{{Mot de passe}}</label>
-                    <div class="col-sm-3">
-                        <input type="password" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="password" placeholder="Mot de passe"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">{{Heure de relève}}</label>
-                    <div class="col-sm-3">
-                        <select class="form-control configuration eqLogicAttr" data-l1key="configuration" data-l2key="heure">
-                        <?php
-                        for ($heure=0; $heure<24; $heure++) {
-                            echo '<option value="'.$heure.'">'.$heure.'H00</option>';
-                        }
-                        ?>
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group" id="group-configuration-depart">
-                    <label class="col-sm-3 control-label">{{Index de départ}}</label>
-                    <div class="col-sm-3">
-                        <input type="number" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="depart" placeholder="0123456789"/>
-                    </div>
-                </div>
+        <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+        <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+        <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
 
-            </fieldset>
-        </form>
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+            <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+            <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+        </ul>
 
-
-        <form class="form-horizontal">
-            <fieldset>
-                <legend>
-                    <i class="fa fa-bullhorn"></i> {{Alertes}}
-                </legend>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">{{Maximum journalier}}</label>
-                    <div class="col-sm-3">
-                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="maxday" placeholder="Max Journalier (litres)"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">{{Maximum mensuel}}</label>
-                    <div class="col-sm-3">
-                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="maxmonth" placeholder="Max mensuel (litres)"/>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">{{Commande}}</label>
-                    <div class="col-sm-3">
-                        <div class="input-group">
-                            <input type="text"  class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="alert" />
-                            <span class="input-group-btn">
-                            <a class="btn btn-default cursor" title="Rechercher une commande" id="bt_selectAlertCmd"><i class="fa fa-list-alt"></i></a>
-                            </span>
+        <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+            <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+                <form class="form-horizontal">
+                    <fieldset>
+                        <div class="form-group" style="margin-top:20px;">
+                            <label class="col-sm-3 control-label">{{Nom du compte Veolia}}</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement Veolia eau}}"/>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </fieldset>
-        </form>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                            <div class="col-sm-3">
+                                <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                                    <option value="">{{Aucun}}</option>
+                                    <?php
+                                    foreach (object::all() as $object) {
+                                        echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Catégorie}}</label>
+                            <div class="col-sm-8">
+                            <?php
+                                foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+                                    echo '<label class="checkbox-inline">';
+                                    echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+                                    echo '</label>';
+                                }
+                            ?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label" ></label>
+                            <div class="col-sm-9">
+                                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+                                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Service}}</label>
+                            <div class="col-sm-3">
+                                <select class="form-control configuration eqLogicAttr" id="configuration_website" data-l1key="configuration" data-l2key="website">
+                                <?php
+                                    echo '<option value="1">Veolia</option>';
+                                    echo '<option value="2">Veolia Méditerranée</option>';
+                                    //echo '<option value="3">Service Eau du Grand Lyon</option>';
+                                    echo '<option value="4">Tout sur mon eau / Eau en ligne</option>';
+                                 ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Identifiant}}</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="login" placeholder="Identifiant"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Mot de passe}}</label>
+                            <div class="col-sm-3">
+                                <input type="password" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="password" placeholder="Mot de passe"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Heure de relève}}</label>
+                            <div class="col-sm-3">
+                                <select class="form-control configuration eqLogicAttr" data-l1key="configuration" data-l2key="heure">
+                                <?php
+                                for ($heure=0; $heure<24; $heure++) {
+                                    echo '<option value="'.$heure.'">'.$heure.'H00</option>';
+                                }
+                                ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group" id="group-configuration-depart">
+                            <label class="col-sm-3 control-label">{{Index de départ}}</label>
+                            <div class="col-sm-3">
+                                <input type="number" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="depart" placeholder="0123456789"/>
+                            </div>
+                        </div>
 
-		<legend>{{Commande}}</legend>
-		<table id="table_cmd" class="table table-bordered table-condensed">
-			<thead>
-				<tr>
-					<th style="width: 50px;">#</th>
-                    <th style="width: 150px;">{{Nom}}</th>
-                    <th style="width: 110px;">{{Sous-Type}}</th>
-                    <th style="width: 150px;">{{Paramètres}}</th>
-                    <th style="width: 150px;"></th>
-				</tr>
-			</thead>
-			<tbody>
+                    </fieldset>
+                </form>
 
-			</tbody>
-		</table>
+                 <legend>{{Alertes}}</legend>
+                <form class="form-horizontal">
+                    <fieldset>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Maximum journalier}}</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="maxday" placeholder="Max Journalier (litres)"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Maximum mensuel}}</label>
+                            <div class="col-sm-3">
+                                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="maxmonth" placeholder="Max mensuel (litres)"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">{{Commande}}</label>
+                            <div class="col-sm-3">
+                                <div class="input-group">
+                                    <input type="text"  class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="alert" />
+                                    <span class="input-group-btn">
+                                    <a class="btn btn-default cursor" title="Rechercher une commande" id="bt_selectAlertCmd"><i class="fa fa-list-alt"></i></a>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
 
-        <form class="form-horizontal">
-            <fieldset>
-                <div class="form-actions">
-                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-                </div>
-            </fieldset>
-        </form>
+            <div role="tabpanel" class="tab-pane" id="commandtab">
+                <table id="table_cmd" class="table table-bordered table-condensed" style="margin-top:20px;">
+                    <thead>
+                        <tr>
+                            <th style="width: 50px;">#</th>
+                            <th style="width: 150px;">{{Nom}}</th>
+                            <th style="width: 110px;">{{Sous-Type}}</th>
+                            <th style="width: 150px;">{{Paramètres}}</th>
+                            <th style="width: 150px;"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 
