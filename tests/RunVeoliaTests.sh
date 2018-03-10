f47d5937031b3b45c165017905d763572d28ff38
Rene Jullien <rene.jullien@gmail.com>
2018-03-10 09:50:53 +0100

    Fix expected outputs

--- tests/RunVeoliaTests.sh
+++ tests/RunVeoliaTests.sh
@@ -7,7 +7,7 @@ cp ./veolia_sudest_data_src/veolia_html_nodata.htm veolia_sudest_data
 cp ./veolia_sudest_data_src/veolia_html_Feb18.htm veolia_sudest_data
 cp ./veolia_sudest_data_src/veolia_html_Feb18_Non_Mesure.htm veolia_sudest_data
 php testVeoliaSudEst.php |grep -v "It is not safe to rely on the system"  > veolia_sudest_data/testVeoliaSudEst.txt
-# cp veolia_sudest_data/testVeoliaSudEst.txt veolia_sudest_data_src
 echo diff -B veolia_sudest_data/testVeoliaSudEst.txt veolia_sudest_data_src/testVeoliaSudEst.txt
+echo cp veolia_sudest_data/testVeoliaSudEst.txt veolia_sudest_data_src
 # grep "displayConfig" veolia_sudest_data/testVeoliaSudEst.txt
 # grep "displayConfig" veolia_sudest_data_src/testVeoliaSudEst.txt
