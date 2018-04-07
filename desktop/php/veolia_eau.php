f5a3a1ae21c909869405ebcd3ad4b037460ecdc7
Rene Jullien <rene.jullien@gmail.com>
2018-04-07 10:47:01 +0200

    Ajout du panel de config dans la vue desktop

--- desktop/php/veolia_eau.php
+++ desktop/php/veolia_eau.php
@@ -30,13 +30,21 @@ $eqLogics = eqLogic::byType($plugin->getId());
     </div>
 
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+        <div class="eqLogicThumbnailContainer">
+        <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+            <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+            <br>
+            <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
+        </div>
+        <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+            <center>
+                <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+            </center>
+            <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
+        </div>
+        </div>
         <legend>{{Mes comptes Veolia}}</legend> <!-- changer pour votre type d'équipement -->
         <div class="eqLogicThumbnailContainer">
-            <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-                <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-                <br>
-                <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
-            </div>
             <?php
                 foreach ($eqLogics as $eqLogic) {
                     $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
