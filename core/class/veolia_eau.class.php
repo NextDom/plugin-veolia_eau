cea2d49d1030a68ab68efbe294365e5d7fdd01a5
Jean-Mercier <25041000+Jean-Mercier@users.noreply.github.com>
2018-05-25 14:20:13 +0200

    fix dependancy problem

--- core/class/veolia_eau.class.php
+++ core/class/veolia_eau.class.php
@@ -19,20 +19,3 @@
 /* * ***************************** Includes ****************************** */
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 require_once dirname(__FILE__) . '/veolia_eau_process.class.php';
-
-class veolia extends eqLogic {
-	public static function dependancy_info() {
-		$return = array();
-		$return['progress_file'] = '/tmp/dependancy_veolia_in_progress';
-		$return['state'] = 'ok';
-		if (exec('apt list --installed php7.0-mbstring | grep -E "mbstring"| wc -l') < 1) {
-			$return['state'] = 'nok';
-		}
-		return $return;
-	}
-	public static function dependancy_install() {
-		log::remove(__CLASS__ . '_update');
-		return array('script' => dirname(__FILE__) . '/../../resources/install_#stype#.sh ' . jeedom::getTmpFolder('veolia') . '/dependance', 'log' => log::getPathToLog(__CLASS__ . '_update'));
-	}
-	
-	}
